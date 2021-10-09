ce31a960bcc528dbd9bb10a7f6d3c5e6059e9747
Wen Wen <wenwen@seagroup.com>
2021-10-09 14:06:33 +0800

    update main

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # CKA-BKP
+echo "# CKA-BKP" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/graywen24/CKA-BKP.git
+git push -u origin main
---
+++ gcp/useful-cli.mk
@@ -0,0 +1,3 @@
+ wenw@C02CK14YLVDN.local  /Users/wenw/Documents/sea/cka/gcp   kubectl config current-context                                                                        15:49:35
+gke_sealabs-poc_asia-southeast1-a_public-gke
+
